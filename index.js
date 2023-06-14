e1b27b15cd32a708c38781c0ad99aa4380c02368
Kumaran-23 <kumz.nair@gmail.com>
2023-06-14 14:55:15 +0800

    updated sign up endpoint to store passwords with hashing using bcrypt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import prisma from "./src/utils/prisma.js"
 import { Prisma } from '@prisma/client'
-
+import bcrypt from "bcryptjs"
 
 const app = express()
 const port = process.env.PORT || 8080
@@ -16,17 +16,49 @@ app.listen(port, () => {
   console.log(`App started; listening on port ${port}`)
 })
 
-app.post('/users', (req, res) => {
-    const {name, email, password} = req.body;
+function filter(obj, ...keys) {
+  return keys.reduce((a, c) => ({ ...a, [c]: obj[c]}), {})
+}
 
+function validateUser(input) {
+  const validationErrors = {}
+
+  if (!('name' in input) || input['name'].length == 0) {
+    validationErrors['name'] = 'cannot be blank'
+  }
+
+  if (!('email' in input) || input['email'].length == 0) {
+    validationErrors['email'] = 'cannot be blank'
+  }
+
+  if (!('password' in input) || input['password'].length == 0) {
+    validationErrors['password'] = 'cannot be blank'
+  }
+
+  if ('password' in input && input['password'].length < 8) {
+    validationErrors['password'] = 'should be at least 8 characters'
+  }
+
+  if ('email' in input && !input['email'].match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    validationErrors['email'] = 'is invalid'
+  }
+
+  return validationErrors
+}
+
+app.post('/users', (req, res) => {
+    const data = req.body;
+    const validationErrors = validateUser(data)
+
+    if (Object.keys(validationErrors).length != 0) return res.status(400).send({
+      error: validationErrors
+    })
+
+    data.password = bcrypt.hashSync(data.password, 8);
     prisma.user.create({
-        data: {
-            name,
-            email,
-            password,
-        }
+        data
     }).then(user => {
-        return res.json(user);
+        return res.json(filter(user, 'id', 'name', 'email'));
     }).catch(err => {
     if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
       const formattedError = {}
