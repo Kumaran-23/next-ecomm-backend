20d69a6620e74715a5ace5d8d4235a607fd89360
Kumaran-23 <kumz.nair@gmail.com>
2023-06-14 10:27:38 +0800

    added user sign up funtion with error handling

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express"
 import prisma from "./src/utils/prisma.js"
+import { Prisma } from '@prisma/client'
+
 
 const app = express()
 const port = process.env.PORT || 8080
+app.use(express.json())
 
 app.get('/', async (req, res) => {
   const allUsers = await prisma.user.findMany()
@@ -11,4 +14,28 @@ app.get('/', async (req, res) => {
 
 app.listen(port, () => {
   console.log(`App started; listening on port ${port}`)
+})
+
+app.post('/users', (req, res) => {
+    const {name, email, password} = req.body;
+
+    prisma.user.create({
+        data: {
+            name,
+            email,
+            password,
+        }
+    }).then(user => {
+        return res.json(user);
+    }).catch(err => {
+    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+      const formattedError = {}
+      formattedError[`${err.meta.target[0]}`] = 'already taken'
+
+      return res.status(500).send({
+        error: formattedError
+      });  // friendly error handling
+    }
+    throw err  // if this happens, our backend application will crash and not respond to the client. because we don't recognize this error yet, we don't know how to handle it in a friendly manner. we intentionally throw an error so that the error monitoring service we'll use in production will notice this error and notify us and we can then add error handling to take care of previously unforeseen errors.
+  })
 })
