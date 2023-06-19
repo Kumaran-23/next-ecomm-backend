b14dc44ad516360d46a3cb312b31367f571822bf
Kumaran-23 <kumz.nair@gmail.com>
2023-06-19 17:02:06 +0800

    added logging and schema for image

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import express from "express"
 import cors from "cors"
 import userRouter from "./src/controllers/users.controllers.js"
 import authRouter from "./src/controllers/auth.controllers.js"
+import morgan from "morgan"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(morgan('combined'))
+
 
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
